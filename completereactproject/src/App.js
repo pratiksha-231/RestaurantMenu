ec73ab2b1e61d932969cbf439d91907936796904
pratiksha-231 <141396681+pratiksha-231@users.noreply.github.com>
2023-08-04 18:49:33 +0530

    Add files via upload

---
+++ completereactproject/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+ import Resturant from './Components/Basics/Resturant';
+// import useState from './Components/Hooks/useState';
+//import useEffect from './Components/Hooks/useEffect';
+//import useReducer from './Components/Hooks/useEffect';
+
+const App = () => {
+  return <Resturant/>
+    };
+
+export default App;
